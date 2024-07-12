f8f8a4b286c77d22d12e14b275d8b20498c3beeb
pancrasio43 <alexgllamosas@gmail.com>
2024-07-12 10:16:41 -0400

    Second Commit

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,8 +2,14 @@
   <div>
     <el-col :span="12">
       <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-        <el-row :gutter="12">
-          <el-col :span="21"> {{todo}} </el-col>
+        <el-row v-if="todo" :gutter="12">
+          <el-col  :span="21"> {{todo}} </el-col>
+          <el-col :span="3">
+            <el-button @click="remove()" type="success" icon="el-icon-check" circle></el-button>
+          </el-col>
+        </el-row>
+        <el-row v-else :gutter="12">
+          <el-col  :span="21"> {{issue}} </el-col>
           <el-col :span="3">
             <el-button @click="remove()" type="success" icon="el-icon-check" circle></el-button>
           </el-col>
@@ -20,7 +26,11 @@ export default {
   props: {
     todo: {
      type: String,
-     required: true
+     required: false
+    },
+    issue: {
+      type: String,
+      required: false
     }
   },
   methods: {
